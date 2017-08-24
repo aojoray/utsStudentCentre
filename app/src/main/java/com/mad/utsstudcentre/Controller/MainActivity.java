9107fb5ebb415c5417b288079f3ee230af6cbb99
aojoray <seonyoung.mun@student.uts.edu.au>
2017-08-24 15:38:02 +1000

    MainActivity connected to Enquiry type

--- app/src/main/java/com/mad/utsstudcentre/Controller/MainActivity.java
+++ app/src/main/java/com/mad/utsstudcentre/Controller/MainActivity.java
@@ -1,7 +1,10 @@
 package com.mad.utsstudcentre.Controller;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.mad.utsstudcentre.R;
 
@@ -9,11 +12,21 @@ import com.mad.utsstudcentre.R;
  * Created by HP on 24/08/2017.
  */
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity {
 
     @Override
-        protected void onCreate( Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-        }
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button newBookingBtn = (Button) findViewById(R.id.new_booking_btn);
+        newBookingBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(v.getContext(), EnquiryTypeActivity.class));
+
+            }
+        });
+
+    }
 }
