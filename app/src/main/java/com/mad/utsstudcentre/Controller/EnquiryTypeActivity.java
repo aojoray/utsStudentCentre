1f305e9987d59f47be128b76464d5908e45d0920
aojoray <seonyoung.mun@student.uts.edu.au>
2017-08-24 15:12:50 +1000

    EnquiryTypeActivity added

---
+++ app/src/main/java/com/mad/utsstudcentre/Controller/EnquiryTypeActivity.java
@@ -0,0 +1,18 @@
+package com.mad.utsstudcentre.Controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mad.utsstudcentre.R;
+
+public class EnquiryTypeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enquiry_type);
+
+
+
+    }
+}
